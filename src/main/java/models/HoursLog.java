6b1ba146c29639b0109eec38a955e211a6039831
Maël <119709626+maeldolium@users.noreply.github.com>
2026-05-28 20:27:38 +0200

    feat(hours): add HoursLog entity

---
+++ src/main/java/models/HoursLog.java
@@ -0,0 +1,33 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hours_logs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HoursLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double hours;
+
+    private String description;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+}
