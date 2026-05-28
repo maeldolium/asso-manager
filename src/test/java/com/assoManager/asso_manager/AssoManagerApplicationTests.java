6aab1aab7fc9704b2b0adb4c23a4a32098fd1f59
Maël <119709626+maeldolium@users.noreply.github.com>
2026-05-28 19:28:30 +0200

    Initial commit

---
+++ src/test/java/com/assoManager/asso_manager/AssoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assoManager.asso_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssoManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
