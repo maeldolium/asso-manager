4bc8c7967f8702bc0299a833da6276c9457ee35f
Maël <119709626+maeldolium@users.noreply.github.com>
2026-05-28 20:06:06 +0200

    feat(members): add Member entity

---
+++ src/main/java/models/Member.java
@@ -0,0 +1,34 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private Boolean cotisationPaid;
+
+    @Column(nullable = false)
+    private Double totalHours = 0.0;
+}
