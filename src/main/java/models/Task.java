9558cd979805bf9a4db6941f29effea43dbb30fc
Maël <119709626+maeldolium@users.noreply.github.com>
2026-05-28 20:26:59 +0200

    feat(tasks): add Task entity

---
+++ src/main/java/models/Task.java
@@ -0,0 +1,31 @@
+package models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private Boolean completed = false;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "project_id")
+    private Project project;
+}
